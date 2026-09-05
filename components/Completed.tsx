82c89d17e3a4a8b1b495c7ccf14616881455b50e
Muppidathi-SS <muppidathimss1113@gmail.com>
2026-09-05 16:36:07 +0530

    feat: implemented the completed task ui with dummy data

--- components/Completed.tsx
+++ components/Completed.tsx
@@ -3,7 +3,8 @@
 import { useState, useEffect } from "react";
 import { RootState } from "@/store/store";
 import { useSelector } from "react-redux";
-import { LibraryAddCheckOutlined } from "@mui/icons-material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import { TODAY_COMPLETED_TASKS } from "@/constants/dummy-data";
 
 export default function Completed() {
   const [mounted, setMounted] = useState(false);
@@ -16,19 +17,128 @@ export default function Completed() {
   const activeColor = mounted ? themeColor : "#ff6b4a";
 
   return (
-    <div className="flex flex-col justify-start items-center py-10 h-full">
-      <h2 className="font-bold text-3xl mb-6" style={{ color: activeColor }}>
-        Completed Tasks
-      </h2>
+    <div className="flex flex-col justify-start items-center py-10 px-5 h-full w-full">
+      <div className="w-full">
+        <div className="flex justify-between border-b border-b-gray-500/20 py-2">
+          <p className="font-medium text-xl">Today</p>
+          <p className="bg-black text-white font-medium flex justify-center items-center rounded-full h-7 w-7">
+            3
+          </p>
+        </div>
+        <div className="flex flex-col gap-3">
+          {TODAY_COMPLETED_TASKS.map((item) => {
+            return (
+              <div
+                key={item.id}
+                className={`flex items-center gap-3 mt-3 pb-5 ${
+                  item.id !== TODAY_COMPLETED_TASKS.length
+                    ? "border-b border-b-gray-500/20"
+                    : ""
+                }`}
+              >
+                <div className="relative h-10 w-10">
+                  <div className="h-10 w-10 rounded-full bg-red-500 text-white text-[20px] flex justify-center items-center">
+                    M
+                  </div>
 
-      <div className="flex flex-col justify-center items-center gap-3 mt-38">
-        <LibraryAddCheckOutlined />
-        <h3 className="font-medium text-2xl" style={{ color: activeColor }}>
-          No Completed Tasks Yet!
-        </h3>
-        <p className="text-gray-500 text-sm">
-          Tasks you complete will show up here.
-        </p>
+                  <CheckCircleIcon
+                    className="absolute -bottom-1 -right-1"
+                    sx={{
+                      color: "green",
+                      fontSize: 18,
+                      backgroundColor: "white",
+                      borderRadius: "50%",
+                    }}
+                  />
+                </div>
+
+                <p>{item.taskName}</p>
+
+                <p className="ml-auto text-gray-500">{item.completedTime}</p>
+              </div>
+            );
+          })}
+        </div>
+        <div className="flex justify-between border-b border-b-gray-500/20 py-2">
+          <p className="font-medium text-xl">Yesterday</p>
+          <p className="bg-black text-white font-medium flex justify-center items-center rounded-full h-7 w-7">
+            6
+          </p>
+        </div>
+        <div className="flex flex-col gap-3">
+          {TODAY_COMPLETED_TASKS.map((item) => {
+            return (
+              <div
+                key={item.id}
+                className={`flex items-center gap-3 mt-3 pb-5 ${
+                  item.id !== TODAY_COMPLETED_TASKS.length
+                    ? "border-b border-b-gray-500/20"
+                    : ""
+                }`}
+              >
+                <div className="relative h-10 w-10">
+                  <div className="h-10 w-10 rounded-full bg-red-500 text-white text-[20px] flex justify-center items-center">
+                    M
+                  </div>
+
+                  <CheckCircleIcon
+                    className="absolute -bottom-1 -right-1"
+                    sx={{
+                      color: "green",
+                      fontSize: 18,
+                      backgroundColor: "white",
+                      borderRadius: "50%",
+                    }}
+                  />
+                </div>
+
+                <p>{item.taskName}</p>
+
+                <p className="ml-auto text-gray-500">{item.completedTime}</p>
+              </div>
+            );
+          })}
+        </div>
+        <div className="flex justify-between border-b border-b-gray-500/20 py-2">
+          <p className="font-medium text-xl">Sep 3, Saturday</p>
+          <p className="bg-black text-white font-medium flex justify-center items-center rounded-full h-7 w-7">
+            4
+          </p>
+        </div>
+        <div className="flex flex-col gap-3">
+          {TODAY_COMPLETED_TASKS.map((item) => {
+            return (
+              <div
+                key={item.id}
+                className={`flex items-center gap-3 mt-3 pb-5 ${
+                  item.id !== TODAY_COMPLETED_TASKS.length
+                    ? "border-b border-b-gray-500/20"
+                    : ""
+                }`}
+              >
+                <div className="relative h-10 w-10">
+                  <div className="h-10 w-10 rounded-full bg-red-500 text-white text-[20px] flex justify-center items-center">
+                    M
+                  </div>
+
+                  <CheckCircleIcon
+                    className="absolute -bottom-1 -right-1"
+                    sx={{
+                      color: "green",
+                      fontSize: 18,
+                      backgroundColor: "white",
+                      borderRadius: "50%",
+                    }}
+                  />
+                </div>
+
+                <p>{item.taskName}</p>
+
+                <p className="ml-auto text-gray-500">{item.completedTime}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
---
+++ constants/dummy-data.ts
@@ -0,0 +1,23 @@
+export const TODAY_COMPLETED_TASKS = [
+  {
+    id: 1,
+    taskName:
+      "Worked on removing multiple vertices in the guardrail centreline.",
+    completedTime: "43 Minutes ago",
+  },
+  {
+    id: 2,
+    taskName: "Implemented route synchronization between Guardrail and Atlas.",
+    completedTime: "1 Hour ago",
+  },
+  {
+    id: 3,
+    taskName: "Fixed the selected route issue in the Guardrail filter.",
+    completedTime: "2 Hours ago",
+  },
+  {
+    id: 4,
+    taskName: "Added validation for creating new guardrail work orders.",
+    completedTime: "3 Hours ago",
+  },
+];
