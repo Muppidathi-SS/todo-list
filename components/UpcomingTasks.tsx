196bdde791f901ce1e4c82ed464a9cfcecd86d91
Muppidathi-SS <muppidathimss1113@gmail.com>
2026-09-08 22:24:39 +0530

    feat: add upcoming tasks ui with dummy data

--- components/UpcomingTasks.tsx
+++ components/UpcomingTasks.tsx
@@ -1,6 +1,17 @@
-const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
+"use client";
+import { UPCOMING_DUMMY_DATA } from "@/constants/upcoming-dummy-data";
+import { RootState } from "@/store/store";
+import { AddCircle, RadioButtonUnchecked } from "@mui/icons-material";
+import { useSelector } from "react-redux";
+import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+
+const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 export default function UpcomingTasks() {
+  const themeColor = useSelector((state: RootState) => state.theme.themeColor);
+  const activeDay = "Tue";
+
   return (
     <>
       <section className="px-5 py-3 flex flex-col h-full">
@@ -12,7 +23,61 @@ export default function UpcomingTasks() {
         </div>
         <div className="flex justify-between border-b border-b-gray-400 mt-4 py-2">
           {days.map((day) => (
-            <div className="bg-gray-400 px-10 py-1 rounded-lg" key={day}>{day} 1</div>
+            <div
+              key={day}
+              className={`px-10 py-1 rounded-lg ${
+                day === activeDay ? "" : "text-gray-500"
+              }`}
+              style={
+                day === activeDay
+                  ? {
+                      backgroundColor: `${themeColor}4D`,
+                      color: themeColor,
+                      fontWeight: 500,
+                    }
+                  : undefined
+              }
+            >
+              {day} 1
+            </div>
+          ))}
+        </div>
+        <div className="mt-2 flex flex-col">
+          {UPCOMING_DUMMY_DATA.filter((day) => !day.isCompleted).map((day) => (
+            <div key={day.id} className="border-b border-b-gray-500/30 py-3">
+              <div>
+                <p className="font-medium">{day.dayLabel}</p>
+              </div>
+              <div>
+                {day.upcomingTasks?.map((task, index) => (
+                  <div
+                    key={index}
+                    className="flex gap-3 py-3 cursor-pointer transition-opacity"
+                  >
+                    <div className="cursor-pointer">
+                      {task.isCompleted ? (
+                        <CheckCircleIcon sx={{ color: "orange" }} />
+                      ) : (
+                        <RadioButtonUncheckedIcon sx={{ color: "gray" }} />
+                      )}
+                    </div>
+                    <p className="font-normal text-[18px]">{task.taskName}</p>
+                  </div>
+                ))}
+              </div>
+              <div
+                className="w-fit px-3 py-1 rounded-md flex gap-3 justify-center items-center mt-3"
+                style={{ backgroundColor: `${themeColor}30` }}
+              >
+                <AddCircle
+                  style={{
+                    fontSize: 30,
+                    color: themeColor,
+                  }}
+                />{" "}
+                Add Task
+              </div>
+            </div>
           ))}
         </div>
       </section>
---
+++ constants/upcoming-dummy-data.ts
@@ -0,0 +1,103 @@
+export const UPCOMING_DUMMY_DATA = [
+  {
+    id: 1,
+    key: "mon",
+    day: "Monday",
+    isCompleted: true,
+    dayLabel: null,
+  },
+  {
+    id: 2,
+    key: "tue",
+    day: "Tuesday",
+    isCompleted: true,
+    dayLabel: null,
+  },
+  {
+    id: 3,
+    key: "wed",
+    day: "Wednesday",
+    isCompleted: false,
+    dayLabel: "Today ‧ Wednesday",
+  },
+  {
+    id: 4,
+    key: "thu",
+    day: "Thursday",
+    isCompleted: false,
+    dayLabel: "Tomorrow ‧ Thursday",
+    upcomingTasks: [
+      {
+        id: 1,
+        isCompleted: true,
+        taskName: "Work on guardrail mapping in atlas.",
+      },
+      {
+        id: 2,
+        isCompleted: true,
+        taskName: "Work on 360 button for sign module.",
+      },
+      {
+        id: 3,
+        isCompleted: false,
+        taskName: "Work on 360 button for sign module.",
+      },
+      {
+        id: 4,
+        isCompleted: false,
+        taskName: "Work on 360 button for sign module.",
+      },
+    ],
+  },
+  {
+    id: 5,
+    key: "fri",
+    day: "Friday",
+    isComplted: false,
+    dayLabel: "10 Sep ‧ Friday",
+    upcomingTasks: [
+      {
+        id: 1,
+        isCompleted: false,
+        taskName: "Work on guardrail mapping in 3dViewer.",
+      },
+      {
+        id: 2,
+        isCompleted: false,
+        taskName: "Work on 360 button for zone module.",
+      },
+    ],
+  },
+  {
+    id: 6,
+    key: "sat",
+    day: "Saturday",
+    isCompleted: false,
+    dayLabel: "11 Sep ‧ Saturday",
+    upcomingTasks: [],
+  },
+  {
+    id: 7,
+    key: "sun",
+    day: "Sunday",
+    isCompleted: false,
+    dayLabel: "12 Sep ‧ Sunday",
+    upcomingTasks: [
+      {
+        id: 1,
+        isCompleted: true,
+        taskName: "Work on guardrail mapping in atlas.",
+      },
+      {
+        id: 2,
+        isCompleted: false,
+        taskName: "Work on zone button for sign module.",
+      },
+      {
+        id: 3,
+        isCompleted: true,
+        taskName: "Work on 360 button for culvert module.",
+      },
+    ],
+  },
+];
