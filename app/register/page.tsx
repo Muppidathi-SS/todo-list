6ec7ae1d044d584111f2fbbaac28eef5db2100cf
Muppidathi-SS <muppidathimss1113@gmail.com>
2026-09-11 21:55:56 +0530

    feat: add registration page with form and navigation

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { Email, Password } from "@mui/icons-material";
+import { Email, Password, Person } from "@mui/icons-material";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
-export default function Login() {
+export default function Register() {
   const router = useRouter();
+
   return (
     <>
       <section className="bg-white h-screen w-full flex justify-center gap-20 items-center">
@@ -20,9 +21,9 @@ export default function Login() {
           />
         </div>
         <div className="border border-gray-100 rounded-xl shadow-2xl w-170 flex flex-col justify-center items-center px-4 py-12">
-          <h1 className="text-[32px] font-medium">Welcome</h1>
+          <h1 className="text-[32px] font-medium">Register</h1>
           <p className="text-gray-400">
-            Please enter your details to start your day!
+            Please enter your details to explore the application!
           </p>
           <div className="flex justify-between items-center border border-gray-300 shadow rounded-lg px-3 w-[80%] gap-3 mt-8">
             <Email
@@ -35,6 +36,19 @@ export default function Login() {
               className="py-4 focus:outline-none focus:border-none w-full"
               type="text"
               placeholder="Enter Your email"
+            />
+          </div>
+          <div className="flex justify-between items-center border border-gray-300 shadow rounded-lg px-3 w-[80%] gap-3 mt-8">
+            <Person
+              style={{
+                fontSize: 30,
+                color: "black",
+              }}
+            />
+            <input
+              className="py-4 focus:outline-none focus:border-none w-full"
+              type="text"
+              placeholder="Enter Your Name"
             />
           </div>
           <div className="flex justify-between items-center border border-gray-300 shadow rounded-lg px-3 w-[80%] gap-3 mt-6">
@@ -50,11 +64,21 @@ export default function Login() {
               placeholder="Enter Your Password"
             />
           </div>
-          <div className="flex justify-end w-[80%] py-3">
-            <p className="font-medium underline">Forgot Password?</p>
+          <div className="flex justify-between items-center border border-gray-300 shadow rounded-lg px-3 w-[80%] gap-3 mt-6">
+            <Password
+              style={{
+                fontSize: 30,
+                color: "black",
+              }}
+            />
+            <input
+              className="py-4 focus:outline-none focus:border-none w-full"
+              type="text"
+              placeholder="Confirm Your Password"
+            />
           </div>
-          <button className="bg-black text-white px-3 py-3 w-[80%] rounded-md text-[21px]">
-            Login
+          <button className="bg-black text-white px-3 py-3 w-[80%] rounded-md text-[21px] mt-6">
+            Register
           </button>
           <p className="py-3 text-gray-500 text-md">or</p>
           <div className="flex justify-center items-center border border-gray-300 shadow rounded-lg px-3 py-3 w-[80%] gap-3">
@@ -64,15 +88,15 @@ export default function Login() {
               width={26}
               height={26}
             />
-            <p>Login with Google</p>
+            <p>Continue with Google</p>
           </div>
           <p className="text-black/80 mt-3">
-            Don't have an Account?{" "}
+            Already have an Account?{" "}
             <span
               className="font-medium text-black underline cursor-pointer"
-              onClick={() => router.push("/register")}
+              onClick={() => router.push("/login")}
             >
-              Register
+              Login
             </span>
           </p>
         </div>
