372b83c445456b7abb9781be2a8c794cbd3b3d88
Muppidathi-SS <muppidathimss1113@gmail.com>
2026-09-11 22:48:14 +0530

    feat: uncomment layout structure in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     <html lang="en" className={`${poppins.variable} h-full antialiased`}>
       <body className={`${poppins.className} min-h-full flex flex-col font-sans`}>
         <StoreProvider>
-          {/* <div className="h-screen w-full flex bg-zinc-50 dark:bg-black">
+          <div className="h-screen w-full flex bg-zinc-50 dark:bg-black">
             <div className="px-2 py-3 h-full w-1/4 bg-[#fcfaf8] transition-all duration-300 flex flex-col items-start border-r border-gray-200 dark:border-zinc-800">
               <Sidebar />
               
@@ -32,7 +32,7 @@ export default function RootLayout({
             <div className="h-full flex-1 bg-white dark:bg-zinc-900 overflow-y-auto">
               {children}
             </div>
-          </div> */}
+          </div>
           {children}
         </StoreProvider>
       </body>
