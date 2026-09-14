a00ff021f1b8610d1224bb7272b1580cd9f44051
Muppidathi-SS <muppidathimss1113@gmail.com>
2026-09-14 15:50:24 +0530

    feat: add Calender label in sidebar

---
+++ app/my-calendar/page.tsx
@@ -0,0 +1,5 @@
+import MyCalendar from "@/components/MyCalendarComponents";
+
+export default function page() {
+  return <MyCalendar/>;
+}
---
+++ components/MyCalendarComponents/index.tsx
@@ -0,0 +1,3 @@
+export default function MyCalendar() {
+  return <h1>Welcome sd</h1>;
+}
